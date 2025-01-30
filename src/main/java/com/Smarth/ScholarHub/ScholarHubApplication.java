f889c7aca6c4598bc4f9b57090d99b65ab52a9ed
Smarth Sharma <smarthsharma74@gmail.com>
2025-01-31 00:42:18 +0530

    First commit and push

---
+++ src/main/java/com/Smarth/ScholarHub/ScholarHubApplication.java
@@ -0,0 +1,14 @@
+
+package com.Smarth.ScholarHub;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ScholarHubApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ScholarHubApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/Smarth/ScholarHub/ScholarHubApplicationTests.java
@@ -0,0 +1,13 @@
+package com.Smarth.ScholarHub;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ScholarHubApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
